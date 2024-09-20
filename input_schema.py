a020f3c2b429cba2ebd02401e89f719956e266f1
Rajdeep Borgohain <150957746+rbgo404@users.noreply.github.com>
2024-09-20 18:16:37 +0530

    Update input_schema.py

--- input_schema.py
+++ input_schema.py
@@ -1,38 +1,14 @@
 INPUT_SCHEMA = {
-    "prompt": {
+    "img_url": {
         'datatype': 'STRING',
         'required': True,
         'shape': [1],
-        'example': ["What is deep learning?"]
+        'example': ["https://storage.googleapis.com/sfr-vision-language-research/BLIP/demo.jpg"]
     },
-    "temperature": {
-        'datatype': 'FP32',
+    "text": {
+        'datatype': 'STRING',
         'required': False,
         'shape': [1],
-        'example': [0.7]
-    },
-    "top_p": {
-        'datatype': 'FP32',
-        'required': False,
-        'shape': [1],
-        'example': [0.1]
-    },
-    "repetition_penalty": {
-        'datatype': 'FP32',
-        'required': False,
-        'shape': [1],
-        'example': [1.18]
-    },
-    "max_tokens": {
-        'datatype': 'INT16',
-        'required': False,
-        'shape': [1],
-        'example': [512]
-    },
-    "top_k":{
-        'datatype': 'INT8',
-        'required': False,
-        'shape': [1],
-        'example': [40]
+        'example': ["a photography of"]
     }
 }
